e510f9248ad0d3013a8eea79986d16b55ba522fb
bal15b <bal15b@acu.edu>
2020-02-12 15:59:03 -0600

    First commit

---
+++ src/main/java/com/blee/HappyTeams.java
@@ -0,0 +1,22 @@
+package com.blee;
+
+/******************************************************************************
+ *  Author : Team D.A.P.
+ *  Class  : Spring 2020 Dr. Reeves CS374
+ *  Task   : Do something for the project
+ *  Date   : 2/14/2020
+ *
+ *  Compile: javac HappyTeams.java
+ *  Execute: java HappyTeams
+ *
+ *
+ ******************************************************************************/
+public class HappyTeams 
+{
+
+    public static void main( String[] args )
+    {
+
+    }
+
+}
---
+++ src/test/java/com/blee/HappyTeamsTest.java
@@ -0,0 +1,26 @@
+package com.blee;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.*;
+
+/******************************************************************************
+ *  Author : bal15b Ben Lamont
+ *  Class  : Spring 2020 Dr. Reeves CS374
+ *  Task   : Do Somehting test
+ *  Date   : 2/14/2020
+ ******************************************************************************/
+
+public class HappyTeamsTest
+{
+
+}
+
+
+
+
+
+
+
+
+
