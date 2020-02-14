a5f1e901ff94611ba8115cbe525c51e1a8741964
bal15b <bal15b@acu.edu>
2020-02-14 03:51:23 -0600

    Get something done
    -Can read from a text file
    -Accept team sizes
    -Can do random swaps

--- src/main/java/com/blee/HappyTeams.java
+++ src/main/java/com/blee/HappyTeams.java
@@ -1,4 +1,7 @@
 package com.blee;
+ 
+import java.util.*;
+import java.io.*;
 
 /******************************************************************************
  *  Author : Team D.A.P.
@@ -14,9 +17,78 @@ package com.blee;
 public class HappyTeams 
 {
 
-    public static void main( String[] args )
+  private static String l[];
+  private static int count;
+
+  public static void main( String[] args )
+  {
+    System.out.println(args[0]);
+  }
+
+    private static void prefs(String f)  throws FileNotFoundException
     {
+      String currentDirectory;
+      currentDirectory = System.getProperty("user.dir");
+
+      f = "test.csv";
+      count = 0;
+
+      FileInputStream fis = new FileInputStream("/"+ currentDirectory + "/src/" + f);
+      Scanner scanner = new Scanner(fis);
+          l = new String[10000];
+
+        if (scanner.hasNextLine())
+        {
+          l[0] = scanner.nextLine();
+          count ++;
+        }
+     
+        while(scanner.hasNextLine())
+        {
+          l[count] = scanner.nextLine();
+          count ++;
+        }
+     
+        scanner.close();
 
     }
 
+    public static void random(int size, int verbose, int numberOfSwaps, int timesRan, int optimal, String fileName) throws FileNotFoundException
+    {
+      
+      HappyTeams.prefs(fileName);
+      
+      String temp;
+      int swap1;
+      int swap2;
+
+      for(int i=0; i<=timesRan; i++)
+      {
+        Random rnd = new Random(i);
+
+        for(int j=0; j<=numberOfSwaps; j++)
+        {
+          swap1 = rnd.nextInt(count);
+          swap2 = rnd.nextInt(count);
+
+          temp = l[swap1];
+          l[swap1] = l[swap2];
+          l[swap2] = temp;
+
+          temp = "";
+
+          if(verbose == 1)
+          {
+            for(int k = 0; k<count; k++)
+            {
+              temp = temp + l[k] + " ";
+            }
+
+          System.out.println(temp);
+          }
+
+        }
+      }
+
+    }
 }
--- src/test/java/com/blee/HappyTeamsTest.java
+++ src/test/java/com/blee/HappyTeamsTest.java
@@ -3,6 +3,8 @@ package com.blee;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import org.junit.*;
+import java.util.*;
+import java.io.*;
 
 /******************************************************************************
  *  Author : bal15b Ben Lamont
@@ -13,7 +15,18 @@ import org.junit.*;
 
 public class HappyTeamsTest
 {
+	HappyTeams h;
+    
+    @Before
+    public void initialize() {
+	h = new HappyTeams();
+    }
 
+    @Test
+    public void test() throws FileNotFoundException
+    {
+    	HappyTeams.random(2,0,1000,5,0,"test.csv");
+    }
 }
 
 
